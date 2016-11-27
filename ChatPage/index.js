d3e983757f700d126c57af6561ffb5b3f59f0d40
maoguy <laodanfeng@hotmail.com>
2016-11-27 23:54:29 +0800

    socket.io

--- ChatPage/index.js
+++ ChatPage/index.js
@@ -1,11 +1,18 @@
 var express = require ("express") ;
 var http = require ("http")
+var socket = require ("socket.io") ;
 
-var app = express() ;
+var app = express () ;
+var server = http.Server (app) ;
+var io = socket (server) ;
+
 app.get ("/" , function (req , res) {
   res.sendFile (__dirname + "/index.html") ;
 }) ;
+io.on ("connection" , function (socket) {
+  console.log ("a user connected") ;
+}) ;
 
-http.Server(app).listen(3000 , function () {
+server.listen(3000 , function () {
   console.log ("listening on *:3000") ;
 }) ;
